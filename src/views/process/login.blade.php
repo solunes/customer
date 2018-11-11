f4056e4d072963406b88a9fc67a0e065e07b9d53
Eduardo Mejia <edumejia30@gmail.com>
2018-11-10 21:35:23 -0400

    Correcciones

--- src/views/process/login.blade.php
+++ src/views/process/login.blade.php
@@ -10,22 +10,15 @@
   <div class="account-profile">
 
     <div class="row">
-      <div class="col-md-3 profile-actual">
-        <div class="profile-description">
-          <h2>Prueba Prueba</h2>
-          <p><span class="p_title">Ciudad: </span>La Paz</p>
-          <p><span class="p_title">Dirección: </span>Av. Prueba #123 Calle Test</p>
-          <p><span class="p_title">NIT: </span>123456789</p>
-          <p><span class="p_title">Razón Social: </span>Prueba</p>
-        </div>
+      <div class="col-md-2 profile-actual">
+
       </div>
-      <div class="col-md-9 edit-profile">
-        <h2>Editar datos de Cuenta</h2>
+      <div class="col-md-8 edit-profile">
+        <h2>Iniciar Sesión</h2>
 
         <form class="account-settings-form" action="{{ url('auth/login') }}" method="post">
         
-          <h5>Cambiar contraseña</h5>
-          <p class="small-paragraph-spacing">Puedes cambiar la contraseña de tu cuenta aqui.</p>
+          <p class="small-paragraph-spacing">Si ya tienes una cuenta, ingresa con tu correo electrónico / carnet de identidad y contraseña.</p>
           <div class="row form-section">
             <div class="col-sm-6 col-md-6">
               <div class="form-group">
@@ -42,7 +35,7 @@
             </div><!-- close .col -->
           </div><!-- close .row -->
           <br>
-          <p><input type="submit" class="btn" value="Ingresar"></p>
+          <p><input type="submit" class="btn btn-site" value="Ingresar"></p>
           
           <hr>
           <br>
