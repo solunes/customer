88ef18bab9c04ab570ea4af94fb13567fbb45f18
Eduardo Mejia <edumejia30@gmail.com>
2020-02-04 19:32:10 -0400

    fix

--- src/app/Listeners/CustomerSubscriptionMonthCreating.php
+++ src/app/Listeners/CustomerSubscriptionMonthCreating.php
@@ -16,7 +16,7 @@ class CustomerSubscriptionMonthCreating
             $customer_subscription_month->status = 'cancelled';
             $customer_subscription_month->save();
         }
-        $sale = \Sales::generateSingleSale($event->parent->user_id, $event->parent_id, 1, 2, 1, $event->parent->nit_name, $event->parent->nit_number, $event->subscription_plan->product_bridge->name.' ('.$event->initial_date.' - '.$event->end_date.')', $event->amount, $event->subscription_plan->product_bridge_id);
+        $sale = \Sales::generateSingleSale($event->parent->user_id, $event->parent->parent_id, 1, 2, 1, $event->parent->parent->nit_name, $event->parent->parent->nit_number, $event->subscription_plan->product_bridge->name.' ('.$event->initial_date.' - '.$event->end_date.')', $event->amount, $event->subscription_plan->product_bridge_id);
         $event->sale_id = $sale->id;
         return $event;
     }
